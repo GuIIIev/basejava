a11be267e62d118ab22dcb251af49aa2ebf6d5fd
GuIIIev <aivanov077@yahoo.com>
2023-05-12 14:44:12 +0300

    fix mistake with [size - 1]

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -52,7 +52,7 @@ public class ArrayStorage {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid() == uuid) {
                 storage[i] = storage[size - 1];
-                storage[size] = null;
+                storage[size - 1] = null;
                 size--;
                 return;
             }
