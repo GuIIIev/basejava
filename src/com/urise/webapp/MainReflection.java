d5b11440229cb685404deb7d35493b478a026851
GuIIIev <aivanov077@yahoo.com>
2023-05-29 22:43:10 +0300

    Add method toString via reflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,15 +3,21 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
+        System.out.println(r.toString());
+        Method method = r.getClass().getMethods()[1];
+
+        System.out.println(method.invoke(r));
         System.out.println(r);
     }
 }
