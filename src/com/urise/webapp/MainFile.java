ade79cb41b060c3c984de42c598a4293b550e562
GuIIIev <aivanov077@yahoo.com>
2023-07-03 21:27:09 +0300

    add recursively traversing and listing filenames in directories and subdirectories

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -15,6 +15,7 @@ public class MainFile {
         } catch (IOException e) {
             throw new RuntimeException("Error", e);
         }
+
         File dir = new File(".\\src\\com\\urise\\webapp");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
@@ -23,10 +24,27 @@ public class MainFile {
                 System.out.println(name);
             }
         }
+
         try (FileInputStream fis = new FileInputStream(filePath)) {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        System.out.println("\n\nSome recursion: \n\n");
+        printAll(dir);
+    }
+
+    public static void printAll(File dir) {
+
+        File[] list = dir.listFiles();
+        if (list != null) {
+            for (File file : list) {
+                if (file.isDirectory()) {
+                    printAll(file);
+                }
+                System.out.println(file.getName());
+            }
         }
     }
 }
