83c4b2b11b170f287beaecc2e7a499dc813a4e3c
GuIIIev <aivanov077@yahoo.com>
2023-08-07 21:29:53 +0300

    implement doWrite and doRead

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -5,24 +5,23 @@ import com.urise.webapp.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-public class DataStreamSerializer implements StreamSerializer {
+public class
+DataStreamSerializer implements StreamSerializer {
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
             Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+            writeWithException(dos, contacts.entrySet(), entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
-            }
-            Map<SectionType, Section> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
+            });
+            writeWithException(dos, r.getSections().entrySet(), entry -> {
                 SectionType sectionType = entry.getKey();
                 Section section = entry.getValue();
                 dos.writeUTF(sectionType.name());
@@ -33,31 +32,23 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        dos.writeInt(((ListSection) section).getItems().size());
-                        List<String> list = ((ListSection) section).getItems();
-                        for (String s : list) {
-                            dos.writeUTF(s);
-                        }
+                        writeWithException(dos, ((ListSection) section).getItems(), dos::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        List<Organisation> listOrganisations = ((OrganisationSection) section).getOrganizations();
-                        dos.writeInt(listOrganisations.size());
-                        for (Organisation organisation : listOrganisations) {
+                        writeWithException(dos, ((OrganisationSection) section).getOrganizations(), organisation -> {
                             dos.writeUTF(organisation.getHomePage().getName());
                             dos.writeUTF(organisation.getHomePage().getUrl());
-                            List<Period> periods = organisation.getPeriod();
-                            dos.writeInt(periods.size());
-                            for (Period period : periods) {
+                            writeWithException(dos, organisation.getPeriod(), period -> {
                                 localDateWriter(dos, period.getStartDate());
                                 localDateWriter(dos, period.getEndDate());
                                 dos.writeUTF(period.getTitle());
                                 dos.writeUTF(period.getDescription());
-                            }
-                        }
+                            });
+                        });
                         break;
                 }
-            }
+            });
         }
     }
 
@@ -67,12 +58,8 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-            int sectionSize = dis.readInt();
-            for (int i = 0; i < sectionSize; i++) {
+            readWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readWithException(dis, () -> {
                 SectionType section = SectionType.valueOf(dis.readUTF());
                 switch (section) {
                     case OBJECTIVE:
@@ -81,34 +68,20 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        int listSectionSize = dis.readInt();
-                        List<String> list = new ArrayList<>();
-                        for (int j = 0; j < listSectionSize; j++) {
-                            list.add(dis.readUTF());
-                        }
-                        resume.addSection(section, new ListSection(list));
+                        resume.addSection(section, new ListSection(getSectionList(dis, dis::readUTF)));
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        List<Organisation> listOrganisations = new ArrayList<>();
-                        int listOrganisationsSize = dis.readInt();
-                        for (int j = 0; j < listOrganisationsSize; j++) {
-                            Link link = new Link(dis.readUTF(), dis.readUTF());
-                            int periodSize = dis.readInt();
-                            List<Period> periods = new ArrayList<>();
-                            for (int k = 0; k < periodSize; k++) {
-                                periods.add(new Period(
+                        resume.addSection(section, new OrganisationSection(getSectionList(dis, () -> new Organisation(
+                                new Link(dis.readUTF(), dis.readUTF()),
+                                getSectionList(dis, () -> new Period(
                                         localDateReader(dis),
                                         localDateReader(dis),
                                         dis.readUTF(),
-                                        dis.readUTF()));
-                            }
-                            listOrganisations.add(new Organisation(link, periods));
-                        }
-                        resume.addSection(section, new OrganisationSection(listOrganisations));
-                        break;
+                                        dis.readUTF()))
+                        ))));
                 }
-            }
+            });
             return resume;
         }
     }
@@ -121,5 +94,40 @@ public class DataStreamSerializer implements StreamSerializer {
 
     private LocalDate localDateReader(DataInputStream dis) throws IOException {
         return LocalDate.of(dis.readInt(), dis.readInt(), dis.readInt());
+    }
+
+    private <T> void writeWithException(DataOutputStream dos, Collection<T> collection, IWriter<T> writer) throws IOException { // интерфейс для вывода элемента коллекции куда то в поток
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            writer.write(item);
+        }
+    }
+
+    private <T> void readWithException(DataInputStream dis, IReader<T> reader) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            reader.read();
+        }
+    }
+
+    interface IWriter<T> {
+        void write(T t) throws IOException;
+    }
+
+    interface IReader<T> {
+        void read() throws IOException;
+    }
+
+    interface IGetter<T> {
+        T get() throws IOException;
+    }
+
+    private <T> List<T> getSectionList(DataInputStream dis, IGetter<T> getter) throws IOException {
+        int size = dis.readInt();
+        List<T> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(getter.get());
+        }
+        return list;
     }
 }
