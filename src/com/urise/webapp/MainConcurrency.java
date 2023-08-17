1d75f15a3f205397c9a947aaeab3baaaa0e88946
GuIIIev <aivanov077@yahoo.com>
2023-08-17 21:16:54 +0300

    practice concurrency

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,12 +1,21 @@
 package com.urise.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private static int counter;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
     private static final Object LOCK = new Object();
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<>(){
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat();
+        }
+    };
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -24,33 +33,45 @@ public class MainConcurrency {
 
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
+//
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
 
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+            Future<Integer> future = executorService.submit(() -> {
+//            Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
                 }
+                latch.countDown();
+                return 5;
             });
-            thread.start();
-            threads.add(thread);
-            thread.join();
+//            thread.start();
+//           threads.add(thread);
         }
 
-        threads.forEach(t -> {
+/*        threads.forEach(t -> {
             try {
                 t.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        });
-        System.out.println(MainConcurrency.counter);
-        //DeadLock practice
+        });*/
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(MainConcurrency.counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
     }
 
     private synchronized void inc() {
 //        synchronized (LOCK) {
-        counter++;
+//        counter++;
+        atomicCounter.incrementAndGet();
 //        }
     }
 }
