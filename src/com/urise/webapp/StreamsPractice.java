18eb2386a16bb80da3b7294648f65e083b7d3f84
GuIIIev <aivanov077@yahoo.com>
2023-08-22 14:26:26 +0300

    hw_12

--- src/com/urise/webapp/StreamsPractice.java
+++ src/com/urise/webapp/StreamsPractice.java
@@ -17,7 +17,7 @@ public class StreamsPractice {
         System.out.println(minValue(values));
         List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 3));
         System.out.println(oddOrEven(list));
-        list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+        list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
         System.out.println(oddOrEven(list));
     }
 
